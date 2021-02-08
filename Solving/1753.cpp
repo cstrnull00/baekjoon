b53eef2e05dee1e0eb413d8c46b2b9acffd7dd2b
김도현 <thack9849@gmail.com>
2021-02-08 22:17:57 +0900

    problem 1753 and 14956 has started.

---
+++ Solving/1753.cpp
@@ -0,0 +1,19 @@
+//https://www.acmicpc.net/problem/1753
+
+#include<iostream>
+
+int button_state(int n, int m) {
+    int cnt = 0;
+
+    return cnt;
+}
+
+int main() {
+    int n, m;
+
+    std::cin >> n >> m;
+
+    std::cout << button_state(n, m);
+
+    return 0;
+}
