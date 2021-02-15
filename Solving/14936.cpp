59ea82fee74486a05ab3153769e125ffe5d88076
김도현 <thack9849@gmail.com>
2021-02-15 19:59:13 +0900

    14936 is solving now

--- Solving/14936.cpp
+++ Solving/14936.cpp
@@ -1,17 +1,21 @@
 //https://www.acmicpc.net/problem/14936
 
 #include<iostream>
+#include<vector>
 
 #define _MAX 100001
 
 int button_state(int n, int m) {
     int cnt = 0;
-    bool state[4] = { false, false, false, false };
+    std::vector<bool> state;
+
+    for(int i = 0; i < n; i++)
+        state.push_back(false);
 
     return cnt;
 }
 
-int main() {
+int main() { 
     int n, m;
 
     std::cin >> n >> m;
