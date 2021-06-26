55812aca26eb6846a7f4b7e905455cd6e3764fca
김도현 <thack9849@gmail.com>
2021-06-26 22:09:26 +0900

    #1920 has solved

---
+++ Solved/1920.cpp
@@ -0,0 +1,50 @@
+//https://www.acmicpc.net/problem/1920
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int binarySearch(int);
+
+vector<int> num;
+
+int main() {
+
+	int N, M, input;
+	vector<int> out;
+	
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &input);
+		num.push_back(input);
+	}
+
+	sort(num.begin(), num.end());
+
+	scanf("%d", &M);
+	for (int i = 0; i < M; i++) {
+		scanf("%d", &input);
+		out.push_back(input);
+	}
+
+	for (int i = 0; i < M; i++) printf("%d\n", binarySearch(out[i]));
+
+	return 0;
+}
+
+int binarySearch(int n) {
+	int left = 0;
+	int right = num.size() - 1;
+
+	while (left <= right) {
+		int mid = (left + right) / 2;
+
+		if (n > num[mid]) left = mid + 1;
+		else if (n < num[mid]) right = mid - 1;
+		else if (n == num[mid]) return 1;
+	}
+
+	return 0;
+}
