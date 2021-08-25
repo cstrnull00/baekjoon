b8d8d7de00d7736bea8a6741563eb7718b60fae4
김도현 <thack9849@gmail.com>
2021-08-25 21:09:41 +0900

    #11279 has solved

---
+++ Solved/11279.cpp
@@ -0,0 +1,32 @@
+//https://www.acmicpc.net/problem/11279
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, x;
+	priority_queue<int> heap;
+
+	cin >> n;
+	while (n--) {
+		cin >> x;
+		if (x == 0) {
+			if (heap.empty()) cout << 0 << '\n';
+			else {
+				cout << heap.top() << '\n';
+				heap.pop();
+			}
+		}
+		else {
+			heap.push(x);
+		}
+	}
+
+	return 0;
+}
