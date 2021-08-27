dd362c90881884c45700a26a06f387cb93f5f974
김도현 <thack9849@gmail.com>
2021-08-27 22:38:55 +0900

    #11726 has solved

---
+++ Solved/11726.cpp
@@ -0,0 +1,19 @@
+//https://www.acmicpc.net/problem/11726
+
+#include <iostream>
+
+using namespace std;
+
+int dp[1001] = { 0, 1, 2, 3 };
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 4; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
